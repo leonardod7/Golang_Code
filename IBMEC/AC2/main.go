3a8af6e441d8c23837e4cfb992b726e9907410c4
leonardod7 <107505958+leonardod7@users.noreply.github.com>
2023-08-22 17:14:31 -0300

    Update trabalho AC2

--- IBMEC/AC2/main.go
+++ IBMEC/AC2/main.go
@@ -7,17 +7,47 @@ import (
 
 func main() {
 
-	// Resposta 1):
+	// Declarando o array para permitir as operações de incluir e excluir contatos
+	var contatos [5]*utils.Contato
+	var opcao int
 
-	// Criar contato
-	contatoOriginal := utils.Contato{Nome: "leo", Email: "leonardo@d7par.com.br"}
-	fmt.Println("Contato original:", contatoOriginal)
+	// Criando interface
+	fmt.Println("1. Contatos Listagem")
+	fmt.Println("2. Adicione um Contato")
+	fmt.Println("3. Exclua um Contato")
+	fmt.Println("4. Logout")
+	fmt.Println("--------------------")
+	fmt.Print("Escolha uma das opções: ")
+	fmt.Scan(&opcao)
 
-	// Alterar email
-	contatoOriginal.ModificarEmail("leonardosantos@d7par.com.br")
-	fmt.Println("Contato atualizado:", contatoOriginal)
-	fmt.Println("--------------------------------------------------")
+	switch opcao {
 
-	// Resposta 2):
+	// Lista de contatos
+	case 1:
+		listaDadosUsuarios := utils.VisualizarListaContatos(contatos)
+		for _, dadoContato := range listaDadosUsuarios {
+			fmt.Println(dadoContato)
+		}
+
+	// se for o comando adicionar
+	case 2:
+		var nome, email string
+		fmt.Println("Digite o Nome:  ")
+		fmt.Scan(&nome)
+		fmt.Println("----------------")
+		fmt.Println("Digite o Email: ")
+		fmt.Scan(&email)
+		contato := utils.Contato{nome, email}
+		utils.AdicionarContato(contato, &contatos)
+		fmt.Println(contatos) // array com os contatos e as posições
+		fmt.Println("Contato inserido")
+
+	// se for para excluir o contato
+	case 3:
+		utils.RemoverContato(&contatos)
+		fmt.Println("Contato excluído")
+
+	}
+	fmt.Println(utils.VisualizarListaContatos(contatos))
 
 }
--- IBMEC/AC2/utils/contato.go
+++ IBMEC/AC2/utils/contato.go
@@ -8,3 +8,34 @@ type Contato struct {
 func (c *Contato) ModificarEmail(emailAtual string) {
 	c.Email = emailAtual
 }
+
+func AdicionarContato(contato Contato, contatos *[5]*Contato) bool {
+
+	for i := 0; i < len(contatos); i++ {
+		if contatos[i] == nil {
+			contatos[i] = &contato
+			return true
+		}
+	}
+	return false
+}
+
+func RemoverContato(contatos *[5]*Contato) {
+	for i := len(contatos) - 1; i >= 0; i-- { //começa do último elemento do array e avança para o começo
+		if contatos[i] != nil {
+			contatos[i] = nil
+			return
+		}
+	}
+}
+
+func VisualizarListaContatos(contatos [5]*Contato) []string {
+	var resultado []string
+	for _, c := range contatos {
+		if c != nil {
+			informacoesContato := "Nome: " + c.Nome + " | Email: " + c.Email
+			resultado = append(resultado, informacoesContato)
+		}
+	}
+	return resultado
+}
